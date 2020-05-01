242b8f283514d4bf9144d0273136f3474a0048a5
Wali Rahman <wali.rahman89@yahoo.com>
2020-05-01 19:25:09 -0400

    Update zigzag_conversion.js

--- zigzag_conversion.js
+++ zigzag_conversion.js
@@ -13,18 +13,6 @@ var convert = function(s, numRows) {
          if(y === numRows - 1) increment = false;
          if(y === 0) increment = true
          y = increment ? y+1 : y-1
-         if(increment) {
-             if (y === numRows-1) {
-                 y--
-             } else y ++;
-         } else {
-             if (y===0) {
-                 y++
-             } else {
-                 y--
-             }
-         }
-        
     }
     return rows.reduce((string, row) => string+ row.join(''), '');
     
