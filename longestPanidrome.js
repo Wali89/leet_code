3fe2e081d14a7a504a569de01a6a912b7b68039c
Wali Rahman <wali.rahman89@yahoo.com>
2020-04-30 21:07:55 -0400

    Create longestPanidrome.js

---
+++ longestPanidrome.js
@@ -0,0 +1,25 @@
+const longestPalindrome = s => {
+  if(!s || s.length <= 1) {
+    return s
+  }
+  let longest = s.substring(0, 1)
+  for(let i = 0; i < s.length; i++) {
+    let temp = expand(s, i, i)
+    if(temp.length > longest.length) {
+      longest = temp
+    }
+    temp = expand(s, i, i + 1)
+    if(temp.length > longest.length) {
+      longest = temp
+    }
+  }
+  return longest
+}
+
+const expand = (s, begin, end) => {
+  while(begin >= 0 && end <= s.length - 1 && s[begin] === s[end]) {
+    begin--
+    end++
+  }
+  return s.substring(begin + 1, end)
+}
